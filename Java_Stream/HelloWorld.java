5d3d0f1bf6c57ee0d976e5955d3ef42c8177f1df
JivanGavhane <jivangavhane123456@gmail.com>
2024-09-29 16:32:44 +0530

    Small change

--- Java_Stream/HelloWorld.java
+++ Java_Stream/HelloWorld.java
@@ -2,6 +2,6 @@ package Java_Stream;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello India"); 
+		System.out.println("hello India Welcome"); 
 	}
 }
